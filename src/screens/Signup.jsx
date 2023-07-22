9f497ff033df32bc6c60e726cbcf435f5784da03
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-07-22 21:49:31 +0100

    Completed UI for signup page

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -10,7 +10,24 @@ const Signup = () => {
 
     return (
         <div className="signup">
+            <div className="center">
+                <div className="header">Get Started!</div>
+                <div className="center_label">You are one step ahead towards recieving due payments and generating invoices instantly.</div>
 
+                <form>
+                    <label htmlFor="">Email</label>
+                    <input type="text" placeholder='Email123@gmail.com' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <label htmlFor="">Phone Number</label>
+                    <input type="text" placeholder='+234 123456789' value={phoneNumber} onChange={(e) => { setPhoneNumber(e.target.value) }} />
+                    <label htmlFor="">Password</label>
+                    <input type="password" placeholder='Password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                    <label htmlFor="">Confirm Password</label>
+                    <input type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} />
+                    <p>By creating an account you agree to all our <br /> <span>Terms and Condition</span></p>
+                    <input type="submit" value="Proceed" />
+                </form>
+            </div>
+            <div className="bottom"></div>
         </div>
     )
 }
