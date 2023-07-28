27488c3aa48052dc6fcfe4cf44e96684171a60b5
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-07-28 22:30:43 +0100

    Worked on Login Page

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import '../styles/login.scss'
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function formSubmit() {
+        navigate("/dashboard")
+    }
     return (
-        <div className="login"></div>
+        <div className="login">
+            <div className="center">
+                <div className="header">Get Started!</div>
+                <div className="center_label">Log Into your account</div>
+
+                <form onSubmit={formSubmit}>
+                    <label htmlFor="">Email</label>
+                    <input type="text" placeholder='Email123@gmail.com' minLength={2} maxLength={255} value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    <label htmlFor="">Password</label>
+                    <input type="password" placeholder='Password' minLength={2} maxLength={255} value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                    <span>Forgot Password?</span>
+                    <input type="submit" value="Proceed" />
+
+                    <p>Don&apos;t have an account? <span>Sign Up</span></p>
+                </form>
+            </div>
+            <div className="bottom"></div>
+        </div>
     )
 }
 
