d42186db3cc1517f627ed7da6b5a41ef058dbeef
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-11-14 09:00:28 +0100

    Installed Tailwind css

--- src/screens/AccountSetup.jsx
+++ src/screens/AccountSetup.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import '../styles/accountsetup.scss'
+import { useNavigate } from 'react-router-dom'
 
 const AccountSetup = () => {
-    const [prefferedCurrency, setPrefferedCurrency] = useState("")
-    const [prefferedCategory, setPrefferedCategory] = useState("")
+    const naviagte = useNavigate()
     const prefferedCurrencies = ["Nigerian Naira(N)", "Ghanian Cedes(c)", "European Euro(€)", "American Dollar($)", ""]
     const prefferedCategories = ["Information Commuinication Technology(ICT)", "Human Resource", ""]
+
+    // Form Values
+    const [firstname, setFirstName] = useState()
+    const [lastname, setLastName] = useState()
+    const [bussinessName, setBussinessName] = useState()
+    const [prefferedCurrency, setPrefferedCurrency] = useState("")
+    const [prefferedCategory, setPrefferedCategory] = useState("")
+    const [bussinessAddress, setBussinessAddess] = useState()
+
+
+    function SubmitForm(e) {
+        e.preventDefault();
+        const formData = new FormData()
+
+    }
+
 
     return (
         <div className="accountSetup">
@@ -15,15 +31,24 @@ const AccountSetup = () => {
                     <div className="center_label">Please fill in the details to complete your account setup.</div>
                 </div>
 
-                <form>
+                <form onSubmit={SubmitForm}>
                     <label htmlFor="">First Name</label>
-                    <input type="text" placeholder='Adamu' />
+                    <input type="text" placeholder='Adamu' value={firstname}
+                        onChange={(e) => { setFirstName(e.target.value) }} minLength={2} maxLength={64} required />
+
+
                     <label htmlFor="">Last Name</label>
-                    <input type="text" placeholder='Bello' />
+                    <input type="text" placeholder='Bello' value={lastname}
+                        onChange={(e) => { setLastName(e.target.value) }} maxLength={64} minLength={2} required />
+
+
                     <label htmlFor="">Bussiness Name</label>
-                    <input type="text" placeholder='Bussiness Name' />
+                    <input type="text" placeholder='Bussiness Name' value={bussinessName}
+                        onChange={(e) => { setBussinessName(e.target.value) }} maxLength={255} minLength={2} required />
+
+
                     <label htmlFor="">Bussiness Category</label>
-                    <select value={prefferedCategory} onChange={(e) => { setPrefferedCategory(e.target.value) }} >
+                    <select title='Bussiness Category' value={prefferedCategory} onChange={(e) => { setPrefferedCategory(e.target.value) }} >
                         {
                             prefferedCategories.map(category => {
                                 return (
