5c5e8b9402aa4dc895b1b6db6de3b7f446c8c0fa
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-12-03 05:54:23 +0100

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-import './Features.scss'
+
 import chartSvg from '../assets/svg/chart-21.svg'
 import messageSvg from '../assets/svg/message.svg'
 import notificationSvg from '../assets/svg/notification.svg'
@@ -6,29 +6,29 @@ import cloudSvg from '../assets/svg/cloud-plus.svg'
 
 const Features = () => {
     return (
-        <div className="features" id="features">
-            <div className="features_section">
-                <div className="features_section-1">
-                    <div className="header1">Our Features</div>
-                    <div className="description">Wehub Invoice Generator helps you get paid on time. We <br></br> provide multiple
+        <div className="h-screen" id="features">
+            <div className="">
+                <div className="">
+                    <div className="">Our Features</div>
+                    <div className="">Wehub Invoice Generator helps you get paid on time. We <br></br> provide multiple
                         payment
                         options that your customers <br></br> can choose from to make their payment securely.</div>
-                    <div className="header2">Payment Modes</div>
+                    <div className="">Payment Modes</div>
                 </div>
-                <div className="features_section-2">
-                    <div className="section_card">
+                <div className="">
+                    <div className="">
                         <img src={messageSvg} alt="" />
                         <h3>Send Invoice via via sms, email or client portal.</h3>
                     </div>
-                    <div className="section_card">
+                    <div className="">
                         <img src={chartSvg} alt="" />
                         <h3>Keep track of customers Invoice status.</h3>
                     </div>
-                    <div className="section_card">
+                    <div className="">
                         <img src={notificationSvg} alt="" />
                         <h3>Follow up with automated <br></br> payment reminders and <br></br> notifications.</h3>
                     </div>
-                    <div className="section_card">
+                    <div className="">
                         <img src={cloudSvg} alt="" />
                         <h3>Thousands of subscribers <br></br> make use of Wehub Invoice <br></br> Generator daily.</h3>
                     </div>
