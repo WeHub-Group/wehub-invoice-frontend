1e4e67ed376ccaf6e0da7d8a4809b2fbe841c084
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-11-11 09:31:37 +0100

    Update on team item

--- src/components/Landing/Team.jsx
+++ src/components/Landing/Team.jsx
@@ -11,17 +11,13 @@ const Team = () => {
                 </div>
 
                 <div className="flex flex-wrap -m-2">
-
-                    <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                            {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" /> */}
-                            <div className="flex-grow">
-                                <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
-                                <p className="text-gray-500">UI Designer</p>
-                            </div>
-                        </div>
-                    </div>
-
+                    <TeamItemLayout name='Ronald Kelechi' position='Senior Fullstack Developer' />
+                    <TeamItemLayout name='Gift Jackson' position='Frontend Developer' />
+                    <TeamItemLayout name='Joshua Umahi' position='Frontend Developer' />
+                    <TeamItemLayout name='Opeoluwa ' position='Backend Developer' />
+                    <TeamItemLayout name='Favour Asun' position='UI/UX Designer' />
+                    <TeamItemLayout name='Kamsi Kelechi' position='UI/UX Designer' />
+                    <TeamItemLayout name='Seyi Aletan' position='Lead Graphic Designer' />
                 </div>
 
             </div>
@@ -29,4 +25,17 @@ const Team = () => {
     )
 }
 
+function TeamItemLayout(params) {
+    return (
+        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+            <div className="h-full flex items-center border-gray-200 hover:border-primary hover:scale-105 transition-all border p-4 rounded-lg">
+                {/* <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" /> */}
+                <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">{params?.name}</h2>
+                    <p className="text-gray-500">{params.position}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 export default Team
